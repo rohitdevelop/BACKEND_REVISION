0ace0d412a686ff7cb65d3536275c51d3107a261
Rohit singh <rohitdev124421@gmail.com>
2025-11-07 15:44:59 +0530

    learnig

--- src/router/userrouter.js
+++ src/router/userrouter.js
@@ -10,20 +10,18 @@ router.get("/", (req, res) => {
 // Handle form submission (Create)
 router.post("/submit", async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+ const {email,name,password} = req.body()
 
-    // Prevent duplicate email
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.render("index", { error: "Email already exists!" });
-    }
+ const exixtmail = await User.findOne({email})
+if (exixtmail) {
+   return res.render("index",{error:"email hai"})
+}    
 
-    // Save new user
-    const newUser = new User({ name, email, password });
-    await newUser.save();
+const people = new User({name ,email, password})
+await people.save()  
 
-    res.redirect("/user-deta");
-  } catch (error) {
+res.redirect('user-deta')
+} catch (error) {
     console.error("Error saving user:", error);
     res.render("index", { error: "Something went wrong, try again!" });
   }
@@ -31,27 +29,27 @@ router.post("/submit", async (req, res) => {
 
 // Read all users
 router.get("/user-deta", async (req, res) => {
-  const users = await User.find();
-  res.render("deta", { users });
+ const deta = await User.find()
+ res.render("deta",{deta})
 });
 
 // Show update form
 router.get("/edit/:id", async (req, res) => {
-  const user = await User.findById(req.params.id);
-  res.render("edit", { user });
+ const user = await User.findById(req.params.id)
+ res.render("edit",{user})
 });
 
 // Handle update (Update)
 router.post("/edit/:id", async (req, res) => {
-  const { name, email } = req.body;
-  await User.findByIdAndUpdate(req.params.id, { name, email });
-  res.redirect("/user-deta");
+  const {name,email} = req.body 
+  await User.findByIdAndUpdate(req.params.id , {name,email})
+  res.redirect("/user-deta",)
 });
 
 // Delete user (Delete)
 router.get("/delete/:id", async (req, res) => {
-  await User.findByIdAndDelete(req.params.id);
-  res.redirect("/user-deta");
+ await User.findByIdAndDelete(req.params.id)
+ res.redirect("/user-deta")
 });
 
 module.exports = router;
